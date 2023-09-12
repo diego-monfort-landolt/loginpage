26717ed042720b90f37a4d420fb56fea7601d0b5
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-12 22:28:24 +0200

    v

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import es from '../imges/es.jpg';
 
 
 const HomePage = () => {
 
-    return(
-    <>
-    <Navbar />
-    <h1>Home</h1>
-    </>
-    ) 
-        
-    
+    const texto = 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid'
+    return (
+        <>
+            <Navbar />
+            <div className='tests'>
+                <h1>Home Page</h1>
+                <p>This is the home page... {texto}</p>
+                <div className='imes'><img src={es} /></div>
+
+
+            </div>
+        </>
+    )
+
+
 }
 
 export default HomePage
