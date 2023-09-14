975ec1f2695013cf147305cb2bd295ae1a3b3298
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-14 20:55:39 +0200

    x
    x

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Navbar'
+import '../stylesheet/dashboard.css'
+import foto from '../imges/bg1.jpg'
 
 const DashboardPage = () => {
- 
+  const h2 = <h2><span className='spanS'>W</span>elcome</h2>;
+  const [texto, setTexto] = useState(true)
+  const cambio = () => {
+    setTexto(!texto)
+  }
+
+
   return (
-  <>
-  <Navbar />
+    <>
+      <Navbar />
 
-    <div className='dashboardContainer'>
-      <h2>Welcom</h2>
-      <p>Welcome to the dashboard!</p>
+      <div className='dashboardContainer'>
+        <div className='dashboardtitleContainer'>
+
+          <p>{h2} to the dashboard!</p>
+          <div className='cardContainer'>
+            <div className='card'>
+              <div className='card-body'>
+                <h3 className='card-title'>DashBoard Card</h3>
+                <img src={foto} className='card-img-top' alt='Card image cap' />
+                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+
+            <div className='cardContainer'>
+              <div className='card'>
+                <div className='card-body'>
+                  <h3 className='card-title'>DashBoard Card</h3>
+                  <img src={foto} className='card-img-top' alt='Card image cap' />
+                  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='btn-group'>
+          <h3>{texto ? 'On' : 'Off'}</h3>
+          <button onClick={cambio}>Click!</button>
+        </div>
+
+      </div>
+
+    </>
 
 
+  )
 
-    </div>
-
-   </>
-   
-
- )
-   
 }
 
 export default DashboardPage
